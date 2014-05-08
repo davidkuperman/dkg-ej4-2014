8077862e3b47c92d06353531dd9a6618eeb5743b
davidkuperman <kuper_7@hotmail.com>
2014-05-08 18:49:25 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eje4::Application.config.secret_key_base = 'bd10cca370d2b6054a30b424d36f1e00d75a50d16ab0147196dde47691f3b3baaafc4eaf34e99e0905a097a1a249988e2afc816b0c68bedeedf4dd48e31f6515'
